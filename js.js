b4a117b9646a1a5ed5b66681cc920a74c7e66160
Song Jae Hyeon <0nocsace@gmail.com>
2021-05-25 20:28:39 +0900

    21/05/25 JS /객체의 value 값 x2 해보기

---
+++ js.js
@@ -0,0 +1,20 @@
+let menu = {
+        width: 200,
+        height: 300,
+        title: "My menu"
+      };
+
+function multiplyNumeric(){
+        for(let key in menu){
+                if (typeof menu[key] == 'number'){
+                        //typeof "[입력값]"
+                        menu[key] = (menu[key]*2);
+                        alert(menu[key]); 
+                }else{
+                alert(menu[key]);
+                }
+        }
+     
+      
+}
+multiplyNumeric(menu);
